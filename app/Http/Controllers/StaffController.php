93ecd2d0677ffd4177aec6ed0b31f23f66fef7ed
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2022-09-27 09:15:00 +0500

    fix

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -11,11 +11,8 @@ class StaffController extends Controller
 
     public function __construct()
     {
-        // $this->middleware('staff-list')->only('index');
-        // $this->middleware('staff-show')->only('show');
-        // $this->middleware('staff-delete')->only('destroy');
-        // $this->middleware('staff-update')->only('update');
-        // $this->middleware('staff-store')->only('store');
+    $this->middleware(['permission:staff-list|staff-show|staff-delete|staff-update|staff-store']);
+
         $this->service = new StaffService;
     }
    
